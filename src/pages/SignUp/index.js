cef6a0055609dfedc1e704cf904cdcddee818960
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-09-02 09:48:29 +0700

    add Sign Up pages

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Button, Gap, Header, TextInput} from '../../components';
+
+const SignUp = ({navigation}) => {
+  return (
+    <View style={styles.page}>
+      <Header title="Sign Up" subTitle="Register and eat" onBack={() => {}} />
+      <View style={styles.container}>
+        <View style={styles.photo}>
+          <View style={styles.borderPhoto}>
+            <View style={styles.photoContainer}>
+              <Text style={styles.addPhoto}>Add Photo</Text>
+            </View>
+          </View>
+        </View>
+        <TextInput label="Full Name" placeholder="Type your full name" />
+        <Gap height={16} />
+        <TextInput
+          label="Email Address"
+          placeholder="Type your email address"
+        />
+        <Gap height={16} />
+        <TextInput label="Password" placeholder="Type your password account" />
+        <Gap height={24} />
+        <Button
+          text="Continue"
+          onPress={() => navigation.navigate('SignUpAddress')}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  container: {
+    backgroundColor: 'white',
+    paddingHorizontal: 24,
+    paddingVertical: 26,
+    marginTop: 24,
+    flex: 1,
+  },
+  borderPhoto: {
+    borderWidth: 1,
+    borderColor: '#8D92A3',
+    width: 110,
+    height: 110,
+    borderRadius: 110,
+    borderStyle: 'dashed',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  photo: {
+    alignItems: 'center',
+    marginTop: 26,
+    marginBottom: 16,
+  },
+  photoContainer: {
+    width: 90,
+    height: 90,
+    borderRadius: 90,
+    padding: 24,
+    backgroundColor: '#F0F0F0',
+  },
+  addPhoto: {
+    fontSize: 14,
+    fontFamily: 'Poppins-Light',
+    color: '#8D92A3',
+    textAlign: 'center',
+  },
+});
