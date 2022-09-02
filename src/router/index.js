98bd98d82b16f239a3c5c1d4c7a89da70b0da229
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-09-02 22:14:17 +0700

    add icon bottom navogation

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,14 @@ import {
   SuccessSignUp,
 } from '../pages';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {BottomNavigator} from '../components';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Order" component={Order} />
       <Tab.Screen name="Profile" component={Profile} />
