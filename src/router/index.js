5035d227e59216a2e06fce3e681bed8489ed1f69
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-09-02 20:59:38 +0700

    add bottom tabs & main app page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {
+  Home,
+  Order,
+  Profile,
   SignIn,
   SignUp,
   SignUpAddress,
   SplashScreen,
   SuccessSignUp,
 } from '../pages';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Order" component={Order} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
 
 const Router = () => {
   return (
@@ -36,6 +51,11 @@ const Router = () => {
       <Stack.Screen
         name="SuccessSignUp"
         component={SuccessSignUp}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
