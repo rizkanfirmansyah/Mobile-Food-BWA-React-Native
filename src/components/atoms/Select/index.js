ecebb05a30fbf654ba1f84bf9fdab49c41de31de
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-09-02 09:48:13 +0700

    add components

---
+++ src/components/atoms/Select/index.js
@@ -0,0 +1,38 @@
+import {Picker} from '@react-native-picker/picker';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Select = () => {
+  return (
+    <View>
+      <Text style={styles.label}>Label Select Option</Text>
+      <View style={styles.input}>
+        <Picker
+        // selectedValue={selectedLanguage}
+        // onValueChange={(itemValue, itemIndex) =>
+        //   setSelectedLanguage(itemValue)
+        // }
+        >
+          <Picker.Item label="Java" value="java" />
+          <Picker.Item label="JavaScript" value="js" />
+        </Picker>
+      </View>
+    </View>
+  );
+};
+
+export default Select;
+
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 16,
+    fontFamily: 'Poppins-Regular',
+    color: '#020202',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#020202',
+    borderRadius: 8,
+    paddingHorizontal: 2,
+  },
+});
